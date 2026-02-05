34f62d31c1e801693219c09c0a03198512666a49
Breninhore <150105050+Breninhore@users.noreply.github.com>
2026-02-05 00:54:12 -0300

    Update WhatsApp number in chamarWhats function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function chamarWhats(produto) {
-  const numero = "+55 21 99852-2302"; // coloque seu número com DDD
+  const numero = "55 21 99169-7363"; // coloque seu número com DDD
 
   const mensagem =
     "Olá! 👋 Vim pelo site *Cadeiras do Índio*.\n\n" +
@@ -7,9 +7,10 @@ function chamarWhats(produto) {
     "Pode me passar mais detalhes, prazo de produção e formas de pagamento?";
 
   const link =
-    "https://wa.me/55 21 99852-2302" + numero + "?text=" + encodeURIComponent(mensagem);
+    "https://wa.me/55 21 99169-7363" + numero + "?text=" + encodeURIComponent(mensagem);
 
   window.open(link, "_blank");
 }
+
 
 
