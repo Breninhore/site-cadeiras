c40be20328164714481bf3e818d54cf35bf7acdc
Breninhore <150105050+Breninhore@users.noreply.github.com>
2026-02-05 00:09:03 -0300

    Update WhatsApp function with new message format

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function chamarWhats(produto) {
-  const numero = "5599999999999"; // troque pelo seu número
-  const mensagem = "Olá! Vi no site e tenho interesse em: " + produto;
+  const numero = "+55 21 99852-2302"; // coloque seu número com DDD
 
-  const link = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensagem);
+  const mensagem =
+    "Olá! 👋 Vim pelo site *Cadeiras do Índio*.\n\n" +
+    "Tenho interesse em *" + produto + "*.\n\n" +
+    "Pode me passar mais detalhes, prazo de produção e formas de pagamento?";
+
+  const link =
+    "p: https://wa.me/qr/SUATKCB6RFNZJ1" + numero + "?text=" + encodeURIComponent(mensagem);
 
   window.open(link, "_blank");
 }
-
